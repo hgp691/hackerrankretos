b2557e93e05e90617038decd582fa242dfbb7ee1
hgp691 <hgp691@gmail.com>
2020-04-13 22:24:40 -0500

    Basic data types reading with 'scanf' and write with 'printf'

--- HackerRankRetosC++/main.cpp
+++ HackerRankRetosC++/main.cpp
@@ -7,9 +7,31 @@
 //
 
 #include <iostream>
+#include <cstdio>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // int, long, char, float, and double,
+//    Int ("%d"): 32 Bit integer
+//    Long ("%ld"): 64 bit integer
+//    Char ("%c"): Character type
+//    Float ("%f"): 32 bit real value
+//    Double ("%lf"): 64 bit real value
+    int entero;
+    long largo;
+    char caracter;
+    float flotante;
+    double doble;
+    
+    scanf("%d %ld %c %f %lf", &entero, &largo, &caracter, &flotante, &doble);
+    
+    printf("%d\n", entero);
+    printf("%ld\n", largo);
+    printf("%c\n", caracter);
+    printf("%f\n", flotante);
+    printf("%lf\n", doble);
+    
     return 0;
 }
+
+
